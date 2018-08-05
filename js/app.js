b358b3acf5184d2984d28e868f1c03da1bfa8b11
Tejas Shinde <shindetejas2011@gmail.com>
2018-08-05 20:24:13 +0530

    Made the score panel and added modal

--- js/app.js
+++ js/app.js
@@ -1,23 +1,14 @@
-/*
- * Create a list that holds all of your cards
- */
 
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
- // swal("Congratulations!!", "Yov've Won", "warning");
 let openCount = 0;
+let solved = 0; let clock;
 let timer = 0;
 let moves = 0;
 let array = [];
 let match = 0;
 let list = $('.card');
+let sound = document.getElementById('myAudio');
 
+//call shuffle function and store the shuffled array in newList
 let newList = shuffle(list);
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -34,23 +25,37 @@ function shuffle(array) {
     return array;
 }
 
-for (let i=0; i<=list.length-1; i++){
-    const elem = document.querySelector('.card');
-    elem.remove();
-}
+let length = list.length-1;
+let i = 0;
 
-for (let i=0; i<=list.length-1; i++){
-    const elem = document.querySelector('.deck');
-    elem.appendChild(newList[i]);
-}
+//Select all the cards in deck, save in elem and remove all cards.
+const elem = document.querySelector('.card');
+elem.remove();
 
+//Add the new shuffled array to the deck
+const newFrag = document.createDocumentFragment();
+const deckElem = document.querySelector('.deck');
+for (i = 0; i<=length; i++){
+    newFrag.appendChild(newList[i]);
+}
+deckElem.appendChild(newFrag);
+
+
+//Call openAndShow when clicked on a card.
 list.click(openAndShow);
+
+//Start timer when first clicked on deck
 $('.deck').one('click', function(){
-    setInterval(myCounter, 1000);
+    clock = setInterval(myCounter, 1000);
 });
 
+//Call restart
+$('.fa-repeat').click(restart);
+
+//Open the card
 function openAndShow(){
     updateMoves();
+    // sound.play();
     array[openCount] = $(this).find('i').attr("class");
     openCount++;
     list.removeClass('reverse');
@@ -61,6 +66,7 @@ function openAndShow(){
     }
 }
 
+//Check if two card matches and if the whole deck is solved.
 function OnlyTwoCards(){
     sameOrNot();
     if(openCount == 2 && match !== 1){
@@ -68,6 +74,11 @@ function OnlyTwoCards(){
         // $('.match').addClass('open show');
     } else if (openCount === 2 && match === 1){
         $('.open.show').addClass('match');
+        solved++;
+        if (solved == 8){
+            clearInterval(clock);
+            result();
+        }
     }
     openCount = 0;
     list.not('.match').bind('click', (openAndShow));
@@ -84,12 +95,61 @@ function sameOrNot(){
 function updateMoves(){
     moves++;
     document.querySelector('.moves').textContent = moves;
+    if(moves === 26 || moves === 36){
+        updateStars();
+    }
+}
+
+//Updates the number of stars.
+function updateStars(){
+    if (moves >= 26 && timer >= 20){
+        $('.stars>li:nth-child(1)').remove();
+
+    } else if((moves >= 36 && timer >= 26) || moves >= 45){
+        $('.stars>li:nth-child(2)').remove();
+    }
 }
 
 function myCounter() {
     timer++;
     document.querySelector(".timer").textContent = timer + " " + "  seconds";
 }
+
+//displays the result
+function result(){
+    let howManyStars = (moves >= 36) ? 'img/star.svg' : (moves >= 26) ? 'img/2-stars-symbol.svg' : 'img/3-stars-symbol.svg';
+    swal({
+        title: "Congratulations!!",
+        buttons: {
+            Again:{
+                text: "Play Again",
+                value: "Again",
+            },
+
+            Cancel:{
+                text: "Cancel",
+                value: "Cancel",
+            },
+        },
+        icon: howManyStars,
+        className: "swal_icon",
+        // text: ""
+        text: "Time: " + " " + timer + " seconds" + ", " + " " + " Moves: " + " " + moves
+    })
+    .then((value) => {
+        switch (value){
+            case "Again": location.reload();
+                          break;
+
+            case "Cancel": break;
+        }
+    });
+}
+
+function restart(){
+    location.reload();
+}
+
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
